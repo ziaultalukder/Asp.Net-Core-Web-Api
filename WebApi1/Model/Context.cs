e5818811ef281a9996d5eb23cca805c2c0a6fe56
ziaultalukder <talukderziaul2@gmail.com>
2019-11-27 10:49:56 +0600

    full project

---
+++ WebApi1/Model/Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi1.Model
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
